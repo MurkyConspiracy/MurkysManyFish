51594704ad4e2c46e7b90176d47ec5ddfd36553b
MurkyConspiracy <nickreeder43@gmail.com>
2025-04-03 18:31:15 -0500

    Initial commit

---
+++ src/main/java/com/disruptioncomplex/MurkysManyFishClient.java
@@ -0,0 +1,12 @@
+package com.disruptioncomplex;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MurkysManyFishClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+
+}
---
+++ src/main/java/com/disruptioncomplex/MurkysManyFishDataGenerator.java
@@ -0,0 +1,11 @@
+package com.disruptioncomplex;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MurkysManyFishDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
